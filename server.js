e18e58e2481f59bbc319c4796cb6360860732637
XIANYUN QI <ucesxyq@ucl.ac.uk>
2018-02-22 10:33:33 -0500

    Getting More Sophisticated

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ var privateKey = fs.readFileSync('/home/studentuser/certs/client-key.pem').toStr
 var certificate = fs.readFileSync('/home/studentuser/certs/client-cert.pem').toString();
 var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
-httpsServer.listen(4443);app.get('/test.html', function (req, res) {
+httpsServer.listen(4443);
+app.get('/:fileName', function (req, res) {
  // run some server-side code
-console.log('test.html requested');
+ var fileName = req.params.fileName;
+console.log(fileName + ' requested');
 // note that __dirname gives the path to the server.js file
-res.sendFile(__dirname + '/test.html');
+res.sendFile(__dirname + '/'+ fileName);
 });
