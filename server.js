90c1e5ea91a9961a05bf3fabd73651286556be43
XIANYUN QI <ucesxyq@ucl.ac.uk>
2018-03-08 10:43:17 -0500

    Testing with AJAX

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express();
 // serve static files - e.g. html, css
 app.use(express.static(__dirname));
-var fileName = req.params.fileName;
-console.log(fileName + ' requested');
-// note that __dirname gives the path to the server.js file
-res.sendFile(__dirname + '/'+ fileName);
-});
+var https = require('https');
+var fs = require('fs');
+var privateKey = fs.readFileSync('/home/studentuser/certs/client-key.pem').toString();
+var certificate = fs.readFileSync('/home/studentuser/certs/client-cert.pem').toString();
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(4443);
